ab2dc87a722dde82e65d976ef848e9cd2de0b3cd
Ehrensverd <mail.eskil@gmail.com>
2021-02-03 17:47:22 +0100

    Add missing implementation of TODO methods

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -9,7 +9,11 @@ public class Message {
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+		// TODO: check for length within boundary
+		//
+		if(payload.length <= 128)	// Ensure payload is less than or equal to 127 bytes
+			this.payload = payload;
+		//
 	}
 
 	public Message() {
@@ -17,31 +21,39 @@ public class Message {
 	}
 
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
 	public byte[] encapsulate() {
-		
+
 		byte[] encoded = null;
-		
+
+
 		// TODO
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
+		//
+		encoded = new byte[128];
+		byte payload_size = (byte)this.payload.length;
+		encoded[0] = payload_size; // Add metadata and encapsulate
+		System.arraycopy(this.payload, 0, encoded, 1, payload_size); // Copy from array to array
+		//
 
 		return encoded;
-		
+
 	}
 
 	public void decapsulate(byte[] received) {
 
 		// TODO
-		// decapsulate the data contained in the received byte array and store it 
+		// decapsulate the data contained in the received byte array and store it
 		// in the payload of this message
-		
+		//
+		this.payload = new byte[(int)received[0]]; // get payload size and make empty payload array
+		System.arraycopy(received, 1, this.payload, 0, this.payload.length); // Copy from array to array
+		//
+
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 	}
 }
