ca48656c200fa0f9d456b23d92d211b1440e4c9b
Ehrensverd <mail.eskil@gmail.com>
2021-02-05 12:10:31 +0100

    Add TODO of marshallString and unmarshallString

--- dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
@@ -10,16 +10,22 @@ public class RPCUtils {
 	// in RPC request and RPC responses
 	// data bytearrays and return byte arrays is according to the 
 	// RPC message syntax [rpcid,parameter/return value]
+
+	/*
+	RPCUtils.java containing utility methods for the unmarshalling and marshalling of the supported data types.
+	The implementation of the marshalling/unmarshalling of booleans is provided and can be used for inspiration.
+	Remember that an integer in Java is 4 bytes.
+	 */
 	
 	public static byte[] marshallString(byte rpcid, String str) {
 
 		byte[] encoded;
 
-		// TODO: marshall RPC identifier and string into byte array
 
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+		byte[] stringByteArray = str.getBytes();
+		encoded = new byte[str.length() + 1]; // Possible trouble
+		encoded[0] = rpcid;
+		System.arraycopy(stringByteArray,0 ,encoded , 1 ,encoded.length );
 
 		return encoded;
 	}
@@ -27,12 +33,10 @@ public class RPCUtils {
 	public static String unmarshallString(byte[] data) {
 
 		String decoded;
+		byte[] temp = new byte[data.length -1];
+		System.arraycopy(data, 1, temp, 0, temp.length);
 
-		// TODO: unmarshall String contained in data into decoded
-
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+		decoded = new String(temp);
 
 		return decoded;
 	}
@@ -40,13 +44,8 @@ public class RPCUtils {
 	public static byte[] marshallVoid(byte rpcid) {
 
 		byte[] encoded;
-
-		// TODO: marshall RPC identifier in case of void type
-
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
-
+		encoded = new byte[1];
+		encoded[0] = rpcid;
 		return encoded;
 
 	}
